7646a2a120de4c5959b5b5f57226b790f5fcc53f
Courtney Fox <courtney.fox@me.com>
2021-02-22 10:13:47 -0500

    created prompt questions

--- index.js
+++ index.js
@@ -2,3 +2,72 @@ const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
 
 const employee = new Employee();
+
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your id?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school do you attend?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "number",
+        message: "What is your office number?"
+      }
+    ]);
+  }
+
+  function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
+
+  promptUser()
+    .then(function(answers) {
+      const html = generateHTML(answers);
+
+      return writeFileAsync("index.html", html);
+    })
+    .then(function() {
+      console.log("Successfully wrote to index.html");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
